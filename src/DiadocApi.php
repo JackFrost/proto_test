9eaa6be188c2133b594358d7eb2e3d3704817e04
Leonid Rasumov <masdzen@gmail.com>
2020-06-11 10:15:31 +0300

    remove all

---
+++ src/DiadocApi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Natrium\Diadoc;
+
+// use Natrium\Diadoc\Model\LoginPassword;
+
+/**
+ * Returns responses for Diadoc routes.
+ */
+class DiadocApi {
+  // $loginPassword = new LoginPassword();
+}
